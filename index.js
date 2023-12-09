78a0a28d047db8d1fb7fa5e6b8c278b82ceddeca
ioangall <gallogian3@hotmail.com>
2023-12-09 20:02:37 +0200

    Associate products with farms

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const methodOverride = require('method-override')
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const Product = require('./models/product')
 
-//Connect to MongoDB
+const Product = require('./models/product')
+const Farm = require('./models/farm')
+
+// Connect to MongoDB
 
 mongoose.connect('mongodb://127.0.0.1:27017/productsApp')
     .then(() => {
@@ -15,20 +17,70 @@ mongoose.connect('mongodb://127.0.0.1:27017/productsApp')
         console.log(`CONNECTION ERROR: ${err}`)
     })
 
-//Set EJS path and view engine, Middleware
+// Set EJS path and view engine, Middleware
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
-//Homepage Route
+// FARM ROUTES
+
+app.get('/farms', async (req,res) => {
+   const farms = await Farm.find({})
+   res.render('farms/index', {farms})
+})
+
+
+app.get('/farms/new', (req,res) => {
+    res.render('farms/new')
+})
+
+app.get('/farms/:id', async (req,res) => {
+    const farm = await Farm.findById(req.params.id).populate('products')
+    res.render('farms/details', {farm})
+ })
+
+ app.delete('/farms/:id', async (req,res) => {
+   const farm = await Farm.findByIdAndDelete(req.params.id)
+   res.redirect('/farms')
+ })
+
+app.post('/farms', async (req,res) => {
+   const farm = new Farm(req.body)
+   await farm.save()
+   res.redirect('/farms')
+})
+
+app.get('/farms/:id/products/new', async (req,res) => {
+    const {id} = req.params
+    const farm = await Farm.findById(id)
+    res.render('products/new', {categories, farm})
+ })
+
+ app.post('/farms/:id/products', async (req,res) => {
+    const {id} = req.params
+    const farm = await Farm.findById(id)
+    const {name, price, category} = req.body
+    const product = new Product({name, price, category})
+    farm.products.push(product)
+    product.farm = farm
+    await farm.save()
+    await product.save()
+    res.redirect(`/farms/${farm._id}`)
+ })
+
+
+// PRODUCT ROUTES
+
+
+// Homepage Route
 
 app.get('/', (req,res) => {
     res.send('Homepage')
 })
 
-//View all products page
+// View all products page
 
 app.get('/products', async (req,res) => {
  const {category} = req.query
@@ -37,20 +89,20 @@ app.get('/products', async (req,res) => {
     res.render('products/index', {products, category})
  } else {
     const products = await Product.find({})
-    res.render('products/index', {products, category: 'ALL'})
+    res.render('products/index', {products, category: 'All'})
  }
 })
 
 const categories = ['fruit','vegetable','dairy']
 
-//Form for adding a new product
+// Form for adding a new product
 
 app.get('/products/new', (req,res) => {
     res.render('products/new', {categories})
    })
 
 
-//Add new product to Database
+// Add new product to Database
 
 app.post('/products', async (req,res) => {
     const newProduct = new Product(req.body)
@@ -59,15 +111,15 @@ app.post('/products', async (req,res) => {
    })
 
 
-//View product details page
+// View product details page
 
 app.get('/products/:id', async (req,res) => {
     const {id} = req.params
-    const product = await Product.findById(id)
+    const product = await Product.findById(id).populate('farm', 'name')
     res.render('products/details', {product})
    })
 
-//Edit product information
+// Edit product information
 
 app.get('/products/:id/edit', async (req,res) => {
     const {id} = req.params
